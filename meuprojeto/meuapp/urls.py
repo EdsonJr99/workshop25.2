f4336513ee991499290a12f6c0601ce9920ecf7d
Edson <jredson525@gmail.com>
2025-09-12 17:49:17 -0300

    ws-api

--- meuprojeto/meuapp/urls.py
+++ meuprojeto/meuapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
 from django.contrib import admin
-from .views import listar_pessoas, criar_pessoa, deletar_pessoa, atualizar_pessoas
+from .views import listar_pessoas, criar_pessoa, deletar_pessoa, atualizar_pessoas, consultaCep
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('consulta', consultaCep, name="consultacep"),
     path('listar', listar_pessoas, name='listar_pessoas'),
     path('criar', criar_pessoa, name='criar_pessoa'),
     path('delete/<int:pk>', deletar_pessoa, name='deletar_pessoa'),
