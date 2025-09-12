f7f11e5e09dac4e969078907655732070ca84668
Edson <jredson525@gmail.com>
2025-09-12 17:06:19 -0300

    conclusao-classbasedviews

--- meuprojeto/appcbv/urls.py
+++ meuprojeto/appcbv/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import HelloView, CachorroListView
+from .views import HelloView, CachorroListView, CachorroCreateView, CachorroUpdadeView, CachorroDeleteView
 
 urlpatterns = [path("", HelloView.as_view(), name="index"),
                path ("listar", CachorroListView.as_view(), name="listar_cachorros"),
+               path("criar", CachorroCreateView.as_view(), name="criar_cachorro"),
+               path('atualizar/<int:pk>', CachorroUpdadeView.as_view(), name="atualizar_cachorro"),
+               path('deletar/<int:pk>', CachorroDeleteView.as_view(), name="deletar_cachorro"),
                ]
--- meuprojeto/appcbv/views.py
+++ meuprojeto/appcbv/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse 
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import Cachorro
+from django.urls import reverse_lazy
+from .forms import CachorroForm
+
 
 
 class HelloView(View):
@@ -13,7 +16,28 @@ class CachorroListView(ListView):
     model = Cachorro
     template_name = "list_dog.html"
     context_object_name = "cachorros"
-    
+
+class CachorroCreateView(CreateView):
+    model = Cachorro   
+    form_class = CachorroForm
+    template_name = "criarcachorro.html"
+    success_url = reverse_lazy("listar_cachorros")
+
+class CachorroUpdadeView(UpdateView):
+    model = Cachorro
+    form_class = CachorroForm
+    template_name = "criarcachorro.html"
+    context_object_name = ("cachorro")
+    success_url = reverse_lazy("listar_cachorros")
+
+
+class CachorroDeleteView(DeleteView):
+    model = Cachorro
+    template_name = ("deletarcachorro.html")
+    context_object_name = ("cachorro")
+    success_url = reverse_lazy("listar_cachorros")
+
+
 
 
 
